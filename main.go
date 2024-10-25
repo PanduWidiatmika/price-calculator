ed9a4d7e4d1a32ca88f58107c343a14b5622dea0
Pandu Widiatmika <190710138@students.uajy.ac.id>
2024-10-25 17:08:52 +0700

    added error handling

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 )
 
 func main() {
-	// prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIndludedPriceJob(fm, taxRate)
-		priceJob.Process()
+		err := priceJob.Process()
+
+		if err!= nil {
+      fmt.Println("Could not process job")
+      fmt.Println(err)
+    }
 	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -4,36 +4,39 @@ import (
 	"fmt"
 
 	"example.com/price-calculator/conversion"
-	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/iomanager"
 )
 
 type TaxIndludedPriceJob struct {
-	IOManager					filemanager.FileManager `json:"-"`
+	IOManager					iomanager.IOManager `json:"-"`
 	TaxRate           float64 `json:"tax_rate"`
 	InputPrices       []float64 `json:"input_prices"`
 	TaxIncludedPrices map[string]string `json:"tax_included_prices"`
 }
 
-func (job *TaxIndludedPriceJob) loadData() {
+func (job *TaxIndludedPriceJob) loadData() error {
 	lines, err := job.IOManager.ReadLines()
 
 	if err!= nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	prices, err := conversion.StringsToFloats(lines)
 
 	if err!= nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	job.InputPrices = prices
+	return nil
 }
 
-func (job *TaxIndludedPriceJob) Process() {
-	job.loadData()
+func (job *TaxIndludedPriceJob) Process() error {
+	err := job.loadData()
+
+	if err!= nil {
+    return err
+  }
 
 	result := make(map[string]string)
 
@@ -43,13 +46,12 @@ func (job *TaxIndludedPriceJob) Process() {
 	}
 
 	job.TaxIncludedPrices = result
-
-	job.IOManager.WriteResult(job)
+	return job.IOManager.WriteResult(job)
 }
 
-func NewTaxIndludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIndludedPriceJob {
+func NewTaxIndludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIndludedPriceJob {
 	return &TaxIndludedPriceJob{
-		IOManager: fm,
+		IOManager: iom,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
